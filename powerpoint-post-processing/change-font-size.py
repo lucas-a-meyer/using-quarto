34d9811311223178b06eb7b6a385ae56c88e85b9
Lucas Meyer <lucas.meyer@microsoft.com>
2022-06-20 09:31:35 -0700

    2022-06-20

---
+++ powerpoint-post-processing/change-font-size.py
@@ -0,0 +1,19 @@
+from pptx.util import Pt
+from pptx import Presentation
+
+prs = Presentation("/home/lucasmeyer/msft/using-quarto/docs/presentation.pptx")
+
+# text_runs will be populated with a list of strings,
+# one for each text run in presentation
+text_runs = []
+
+for slide in prs.slides:
+    for shape in slide.shapes:
+        if not shape.has_text_frame:
+            continue
+        for paragraph in shape.text_frame.paragraphs:
+            for run in paragraph.runs:
+                if run.font.name == "Consolas":
+                    run.font.size = Pt(12)
+
+prs.save("/home/lucasmeyer/msft/using-quarto/docs/presentation-fixed.pptx")
