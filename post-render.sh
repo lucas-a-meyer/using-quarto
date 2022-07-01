43bd6cdb4cc4379acf7ec1fbf46f7f86607bf7f2
Lucas Meyer <lucas.meyer@microsoft.com>
2022-07-01 13:00:19 -0700

    Work done by Fri Jul  1 13:00:19 PDT 2022

--- post-render.sh
+++ post-render.sh
@@ -1,1 +1,10 @@
+# reduce font size on PowerPoint
+python3 powerpoint-post-processing/change-font-size.py
+
+# The previous step will generate a file called presentation-fixed.pptx
+# We'll swap that name around
+mv docs/presentation.pptx docs/presentation-orig.pptx
+mv docs/presentation-fixed.pptx docs/presentation.pptx
+
+# Push to GitHub and GitHub pages
 git add .;git commit -m "Work done by `date`";git push
